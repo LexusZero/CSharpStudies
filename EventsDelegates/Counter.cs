0fe3f1eb44cf6aa4acdddff2edc0447d047a6514
xill47 <x-i-ll@hotmail.com>
2021-10-12 14:38:18 +0200

    Events and delegates part 1

---
+++ EventsDelegates/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegates
+{
+    delegate void CounterEvent(int count);
+
+    class Counter
+    {
+        public event CounterEvent EvenCount;
+
+        private int count = 0;
+
+        public void Increment()
+        {
+            count++;
+            Console.WriteLine(count);
+            if (count % 2 == 0)
+            {
+                EvenCount?.Invoke(count);
+            }
+        }
+    }
+}
---
+++ EventsDelegates/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace EventsDelegates
+{
+    delegate double Calculation(double a, double b);
+
+    class Program
+    {
+        static void MainWithEvents()
+        {
+            Counter counter = new Counter();
+            counter.EvenCount += Counter_EvenCount;
+            counter.EvenCount += Counter_EvenCount2;
+            for (int i = 0; i < 8; i++)
+            {
+                counter.Increment();
+            }
+            counter.EvenCount -= Counter_EvenCount2;
+            for (int i = 0; i < 5; i++)
+            {
+                counter.Increment();
+            }
+        }
+
+
+        private static void Counter_DivisibleByThree(int count)
+        {
+            Console.WriteLine("Count is divisible by three");
+        }
+
+        private static void Counter_EvenCount2(int count)
+        {
+            Console.WriteLine("But what is the value");
+        }
+
+        private static void Counter_EvenCount(int count)
+        {
+            Console.WriteLine("Count was even!");
+        }
+
+        // Функции высшего порядка
+        static void PrintCalculation(Calculation calculation, double a, double b)
+        {
+            string calculationName = calculation.Method.Name;
+            Console.WriteLine($"We are doing {calculationName} on a = {a} and b = {b}");
+            double result = calculation(a, b);
+            Console.WriteLine($"The result is {result}");
+        }
+
+        static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Substraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Addition(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        static void PrintNumber(double a)
+        {
+            Console.Write(a);
+        }
+    }
+}
